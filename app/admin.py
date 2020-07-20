d79452f6441c56911390f858ff91e4fb65da9505
Nima Modares Gorji <nima.mg@gmail.com>
2020-07-20 21:04:39 +0430

    Add admin panels

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from app.models import *
+
+admin.site.register(TestDescription)
+admin.site.register(Address)
+admin.site.register(Appointment)
+admin.site.register(Expert)
+admin.site.register(LabTest)
+admin.site.register(Laboratory)
+admin.site.register(Patient)
+admin.site.register(TimeSlot)
