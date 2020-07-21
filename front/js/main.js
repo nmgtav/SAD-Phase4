08b04f44fa64e916e1923e4546547807c6c78367
Bardia Eghbali <bardia.eghbali@gmail.com>
2020-07-21 15:29:53 +0430

    more frontend support

--- front/js/main.js
+++ front/js/main.js
@@ -98,6 +98,22 @@
     time: 1000
   });
 
+  var testDescriptions;
+  var labsAndPrices;
+  var patientAddresses;
+  var availableTimeslots;
+
+  function findWithId(jsonObject, id) {
+    id = parseInt(id);
+    var returnVal = null;
+    jsonObject.forEach(listItem => {
+      if(id == listItem.id) {
+        returnVal = listItem;
+      }
+    });
+    return returnVal;
+  }
+
   // Porfolio isotope and filter
   $(window).on('load', function () {
 
@@ -109,6 +125,7 @@
       // Begin accessing JSON data here
       var data = JSON.parse(this.response);
       if (request.status >= 200 && request.status < 400) {
+        testDescriptions = data;
         data.forEach(test => {
           const item = document.createElement('label');
           item.setAttribute('class', 'item');
@@ -144,22 +161,6 @@
     });
   });
 
-  // Testimonials carousel (uses the Owl Carousel library)
-  $(".testimonials-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    items: 1
-  });
-
-  // Clients carousel (uses the Owl Carousel library)
-  $(".clients-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    responsive: { 0: { items: 2 }, 768: { items: 4 }, 900: { items: 6 }
-    }
-  });
 
   $( "#sendTests" ).click(function() {
     var request = new XMLHttpRequest();
@@ -174,6 +175,7 @@
       const container = document.getElementById('labs');
       var data = JSON.parse(this.response);
       if (request.status >= 200 && request.status < 400) {
+        labsAndPrices = data;
         data.forEach(lab => {
           const item = document.createElement('label');
           item.setAttribute('class', 'item');
@@ -217,6 +219,50 @@
       const container = document.getElementById('addresses');
       var data = JSON.parse(this.response);
       if (request.status >= 200 && request.status < 400) {
+        patientAddresses = data;
+        data.forEach(address => {
+          const item = document.createElement('label');
+          item.setAttribute('class', 'item');
+          item.textContent = address.address;
+
+          const radio = document.createElement('input');
+          radio.setAttribute('type', 'radio');
+          radio.setAttribute('name', 'address');
+          radio.setAttribute('value', address.id);
+          const checkmark = document.createElement('span');
+          checkmark.setAttribute('class', 'radio');
+
+          container.appendChild(item);
+          item.appendChild(radio);
+          item.appendChild(checkmark);
+
+          $('#labSection').addClass('hidden');
+          $('#addressSection').removeClass('hidden');
+        });
+      } else {
+        const errorMessage = document.createElement('marquee');
+        errorMessage.textContent = `Gah, it's not working!`;
+        app.appendChild(errorMessage);
+      }
+    }
+
+    request.send();
+  });
+
+  $( "#newAddress" ).click(function() {
+    var request = new XMLHttpRequest();
+    var selectedTests = $('#tests input:checked');
+    var str = selectedTests[0].getAttribute('name');
+    for (var i = 1; i < selectedTests.length; i++) {
+      str += ',' + selectedTests[i].getAttribute('name');
+    }
+    request.open('GET', 'http://127.0.0.1:8000/api/addresses', true);
+    request.onload = function () {
+      // Begin accessing JSON data here
+      const container = document.getElementById('addresses');
+      var data = JSON.parse(this.response);
+      if (request.status >= 200 && request.status < 400) {
+        patientAddresses = data;
         data.forEach(address => {
           const item = document.createElement('label');
           item.setAttribute('class', 'item');
@@ -255,6 +301,7 @@
       const container = document.getElementById('timeslots');
       var data = JSON.parse(this.response);
       if (request.status >= 200 && request.status < 400) {
+        availableTimeslots = data;
         data.forEach(timeslot => {
           const item = document.createElement('label');
           item.setAttribute('class', 'item');
@@ -270,10 +317,9 @@
           container.appendChild(item);
           item.appendChild(radio);
           item.appendChild(checkmark);
-
-          $('#addressSection').addClass('hidden');
-          $('#timeslotSection').removeClass('hidden');
         });
+        $('#addressSection').addClass('hidden');
+        $('#timeslotSection').removeClass('hidden');
       } else {
         const errorMessage = document.createElement('marquee');
         errorMessage.textContent = `Gah, it's not working!`;
@@ -282,6 +328,88 @@
     }
 
     request.send();
+  });
+
+  var paymentURL;
+
+  $( "#sendRequest" ).click(function() {
+    var request = new XMLHttpRequest();
+    var selectedLab = $("input:radio[name ='lab']:checked").val();
+    var selectedTimeSlot = $("input:radio[name ='timeslot']:checked").val();
+    var selectedAddress = $("input:radio[name ='address']:checked").val();
+    var requestData = {
+        laboratory: selectedLab,
+        time_slot: selectedTimeSlot,
+        address: selectedAddress,
+        tests: []
+    };
+    var selectedTests = $('#tests input:checked');
+    for (var i = 0; i < selectedTests.length; i++) {
+      requestData.tests.push(selectedTests[i].getAttribute('name'));
+    }
+    request.open('POST', 'http://127.0.0.1:8000/api/test-requests/', true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.onload = function () {
+      // Begin accessing JSON data here
+      const container = document.getElementById('requestDetails');
+      var data = JSON.parse(this.response);
+      if (request.status >= 200 && request.status < 400) {
+        paymentURL = data.payment_redirect_url;
+        const p1 = document.createElement('p');
+        p1.textContent = 'Laboratory: ' + findWithId(labsAndPrices, requestData.laboratory).name;
+        container.appendChild(p1);
+        const p4 = document.createElement('p');
+        p4.textContent = 'Tests: \n';
+        container.appendChild(p4);
+        const list = document.createElement('ul');
+        for (var i = 0; i < requestData.tests.length; i++) {
+          const item = document.createElement('li');
+          item.textContent = findWithId(testDescriptions,requestData.tests[i]).name;
+          list.appendChild(item);
+        }
+        container.appendChild(list);
+
+        const p3 = document.createElement('p');
+        p3.textContent =  'Appointment: ' + findWithId(availableTimeslots, requestData.time_slot).date;
+        p3.textContent += ' At ' + findWithId(patientAddresses, requestData.address).address;
+        container.appendChild(p3);
+
+        const p2 = document.createElement('p');
+        p2.textContent =  'Total: ' + data.price + 'T';
+        container.appendChild(p2);
+
+
+        $('#timeslotSection').addClass('hidden');
+        $('#detailsSection').removeClass('hidden');
+      } else {
+        const errorMessage = document.createElement('marquee');
+        errorMessage.textContent = `Gah, it's not working!`;
+        app.appendChild(errorMessage);
+      }
+    }
+
+    request.send(JSON.stringify(requestData));
+  });
+
+  $( "#payButton" ).click(function() {
+    var request = new XMLHttpRequest();
+    console.log(paymentURL);
+    request.open('GET', paymentURL+'1', true);
+    request.onload = function () {
+      // Begin accessing JSON data here
+      const container = document.getElementById('requestDetails');
+      var data = JSON.parse(this.response);
+      if (request.status >= 200 && request.status < 400) {
+        $('#detailsSection h3').textContent = 'Appointment Booked';
+        $( "#payButton" ).addClass('hidden');
+      } else {
+        const errorMessage = document.createElement('marquee');
+        errorMessage.textContent = `Gah, it's not working!`;
+        app.appendChild(errorMessage);
+      }
+    }
+
+    request.send(JSON.stringify(requestData));
   });
 
 
