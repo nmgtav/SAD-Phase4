5034568a2beb85fb212d8bff3354cc6be3852946
Nima Modares Gorji <nima.mg@gmail.com>
2020-07-21 22:29:00 +0430

    Add get list of labs and get patient functions to DAO

--- app/dao.py
+++ app/dao.py
@@ -1,7 +1,15 @@
-from app.models import TestDescription
+from app.models import TestDescription, Laboratory, Patient
 
 
 class DAO:
     @staticmethod
     def get_list_of_all_test_descriptions():
         return TestDescription.objects.all()
+
+    @staticmethod
+    def get_list_of_labs():
+        return Laboratory.objects.all()
+
+    @staticmethod
+    def get_patient(patient_id):
+        return Patient.objects.get(id=patient_id)
